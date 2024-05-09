2fc661feaa224e83b6455e93cadc6e24cc82c41c
Rudrakumar Patel <rdgitwork@gmail.com>
2024-05-08 23:30:47 -0400

    Making environment.

---
+++ anime-blog/public/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./public/**/*.html", // includes all HTML files in the public directory
+        "./public/js/**/*.js", // includes all JavaScript files in the public directory
+        // You can add more paths as needed
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
